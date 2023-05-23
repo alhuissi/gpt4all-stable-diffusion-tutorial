04b1f80ef6b7e3da29bbf67f2011a5e572fe1140
Alfredo <alfredolhuissier@gmail.com>
2023-05-22 21:04:46 -0500

    screenshot

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# This is a resporitory for the GPT4All + stable Diffusion API tutorial
+## This is a resporitory for the GPT4All + stable Diffusion API tutorial
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import dotenv_values
 import gpt4all
